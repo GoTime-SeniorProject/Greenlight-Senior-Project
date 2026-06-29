3c2addf80571e1d9a958e4ec19de2d4164356fca
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 18:39:23 -0400

    move and reinstate express index

---
+++ data-access/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import serverless from "serverless-http";
+import { buildSchema } from "./init/schema.js";
+
+const app = express();
+
+let schemaPromise = buildSchema();
+
+app.get("/health", (req, res) => {
+    res.json({ ok: true });
+});
+
+app.get("/schema", async (req, res) => {
+    const schema = await schemaPromise;
+    res.json(schema);
+});
+
+export default serverless(app);
