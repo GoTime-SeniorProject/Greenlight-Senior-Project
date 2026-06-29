ef18d537673b876477a2a106fb42dd21ca4f896c
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 17:57:43 -0400

    try moving api into src

--- data-access/src/api/graphql.ts
+++ data-access/src/api/graphql.ts
@@ -1,6 +1,6 @@
 import { createHandler } from 'graphql-http/lib/use/express';
-import { buildSchema } from '../src/init/schema.js';
-import { getDb } from '../src/db/mongo-client.js';
+import { buildSchema } from '../init/schema.js';
+import { getDb } from '../db/mongo-client.js';
 import type { Request, Response } from 'express';
 
 export default async function handler(req: Request, res: Response) {
