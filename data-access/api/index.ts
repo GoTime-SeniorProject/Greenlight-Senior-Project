2f915f8958a0e0d9abdfc8e0aa08e8924ef6b7bf
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 18:27:42 -0400

    test deployment

--- data-access/api/index.ts
+++ data-access/api/index.ts
@@ -1,18 +1,10 @@
 import express from "express";
 import serverless from "serverless-http";
-import { buildSchema } from "../src/init/schema.js";
 
 const app = express();
 
-let schemaPromise = buildSchema();
-
-app.get("/health", (req, res) => {
+app.get("/", (req, res) => {
     res.json({ ok: true });
-});
-
-app.get("/schema", async (req, res) => {
-    const schema = await schemaPromise;
-    res.json(schema);
 });
 
 export default serverless(app);
