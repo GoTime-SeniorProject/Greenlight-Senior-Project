1c1cb571660f4585bad61c6f05d40640dc451426
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 19:18:57 -0400

    test conversion PLEASE

--- data-access/src/init/schema.ts
+++ data-access/src/init/schema.ts
@@ -1,24 +1,14 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { resolvers } from '../graphql/resolvers.js';
-import path from 'node:path';
-import { loadSchemaSync } from '@graphql-tools/load';
-import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-
-
-const schema = loadSchemaSync(
-  path.resolve(process.cwd(), 'src/graphql/schema/**/*.graphql'),
-  {
-    loaders: [new GraphQLFileLoader()],
-  }
-);
+import { typeDefs } from '../graphql/schema/schema.js';
 
 export async function buildSchema() {
 
-  console.log('TYPEDEFS:', schema);
+  console.log('TYPEDEFS:', typeDefs);
   console.log('RESOLVERS:', resolvers);
 
   return makeExecutableSchema({
-    typeDefs: schema,
-    resolvers,
+    typeDefs: typeDefs,
+    resolvers: resolvers,
   });
 }
