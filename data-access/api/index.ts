61b906fc08e7ef396e019ca621a71c9a4ec2b7cc
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 18:29:38 -0400

    test

--- data-access/api/index.ts
+++ data-access/api/index.ts
@@ -4,7 +4,11 @@ import serverless from "serverless-http";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.json({ ok: true });
+  res.json({ ok: true });
+});
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
 export default serverless(app);
