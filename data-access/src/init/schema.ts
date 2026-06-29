416f21ebe2095aa14b04a3ca7dccdbb58c827c87
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 18:57:45 -0400

    please fix

--- data-access/src/init/schema.ts
+++ data-access/src/init/schema.ts
@@ -2,11 +2,19 @@ import { loadFiles } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { resolvers } from '../graphql/resolvers.js';
+import path from 'node:path';
+
 
 export async function buildSchema() {
-  const typesArray = await loadFiles('./src/graphql/schema/**/*.graphql');
+  const typesArray = await loadFiles(
+    path.resolve(process.cwd(), 'src/graphql/schema/**/*.graphql')
+  );
   const typeDefs = mergeTypeDefs(typesArray);
+
+  console.log('LOADED TYPEFILES:', typesArray.map(String));
+  
   console.log('TYPEDEFS:', typesArray);
   console.log('RESOLVERS:', resolvers);
+
   return makeExecutableSchema({ typeDefs, resolvers });
 }
