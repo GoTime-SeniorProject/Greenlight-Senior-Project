ddd3f9daf6bd6493b5ffd9034dd78d5908cb5fb8
Olivia Knestaut <olivia.knestaut@gmail.com>
2026-06-29 21:51:28 -0400

    attempt 10 million

--- data-access/src/graphql/schema/index.ts
+++ data-access/src/graphql/schema/index.ts
@@ -1,10 +1,8 @@
 import path from "path";
-import { loadSchemaSync } from "@graphql-tools/load";
-import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { loadFilesSync } from "@graphql-tools/load-files";
 
-export const typeDefs = loadSchemaSync(
-    path.join(process.cwd(), "src/graphql/schema"),
-    {
-        loaders: [new GraphQLFileLoader()],
-    }
+const typesArray = loadFilesSync(
+    path.join(process.cwd(), "src/graphql/schema/**/*.graphql")
 );
+
+export const typeDefs = typesArray;
